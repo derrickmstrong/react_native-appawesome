5ead11626bdf11c1fd272e25287d6eae8dac073d
Derrick Strong <derrickmstrong@gmail.com>
2021-02-14 18:35:05 -0600

    Adding useState Hook

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
@@ -16,10 +8,35 @@ import {
 } from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('Derrick');
+  const [session, setSession] = useState({number: 7, title: 'state'});
+  const [current, setCurrent] = useState(true);
+  
+  const handleName = () => {
+    setName('D.Strong')
+  }
+
+  const handleSession = () => {
+    setSession({number: 8, title: 'styles'})
+  }
+
+  const handleCurrent = () => {
+    setCurrent(!current)
+  }
+
   return (
     <View style={styles.body}>
-    <Text style={styles.text}>Hi, I'm Derrick!</Text>
-    <Button title='Learn More' onPress={()=>{Linking.openURL('http://www.derrickstrong.com')}}></Button>
+      <Text style={styles.text}>Hi, I'm {name}!</Text>
+      {/* <Button title='Learn More' onPress={()=>{Linking.openURL('http://www.derrickstrong.com')}}></Button> */}
+      <Button title="Update Name" onPress={handleName}></Button>
+      <Text style={styles.text}>
+        Session: {session.number} - About: {session.title}
+      </Text>
+      <Button title="Update Session" onPress={handleSession}></Button>
+      <Text style={styles.text}>
+        Status: {current ? 'Current Session' : 'Next Session'}
+      </Text>
+      <Button title="Update Current" onPress={handleCurrent}></Button>
     </View>
   );
 };
@@ -30,13 +47,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#006fff',
     alignItems: 'center',
     justifyContent: 'center',
-    
   },
   text: {
     color: 'orange',
     fontSize: 25,
     margin: 10,
-  }
+  },
 });
 
 export default App;
